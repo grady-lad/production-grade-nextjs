4eb01b83fb33b1a47283290ca67a493a8757c62a
Martin O'Grady <ogradymartin7@gmail.com>
2021-02-11 11:46:18 +0100

    Lession: Connecting a DB.

--- db/connect.ts
+++ db/connect.ts
@@ -9,11 +9,27 @@ import { Db, MongoClient } from 'mongodb'
  * DB connections. Most traditional DBs were not made for a stateless
  * environment like serverless. A serverless DB (HTTP based DB) whould work
  * better.
+ *
+ * The DB will be cached. Even though we are using serverless functions AWS will most likely spin up
+ * the functions within the same container which is where this DB gets cached to.
  */
 global.mongo = global.mongo || {}
 
 export const connectToDB = async () => {
-  const db = {}
+  if (!global.mongo.client) {
+    global.mongo.client = new MongoClient(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferMaxEntries: 0,
+      connectTimeoutMS: 10000,
+    })
+
+    console.log('connecting to DB')
+    await global.mongo.client.connect()
+    console.log('connected to DB')
+  }
+
+  const db: Db = global.mongo.client.db('known')
 
   return { db, dbClient: global.mongo.client }
 }
